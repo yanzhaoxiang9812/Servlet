86c1ad921ad10003dec5a8c08a39b8b1fdbc318b
闫兆祥 <963614893@qq.com>
2021-05-14 16:54:57 +0800

    demo

--- demo4/src/main/java/HttpSessionTest/Servlet5.java
+++ demo4/src/main/java/HttpSessionTest/Servlet5.java
@@ -9,7 +9,9 @@ import java.io.IOException;
 public class Servlet5 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //  通过请求对象，获取当前用户的HttpSession
         HttpSession session = request.getSession();
+        //  通过HttpSession对象添加元素
         session.setAttribute("key1",123);
         session.setAttribute("key2","fuck");
         session.setAttribute("key3",true);
--- demo4/src/main/java/HttpSessionTest/Servlet6.java
+++ demo4/src/main/java/HttpSessionTest/Servlet6.java
@@ -9,7 +9,9 @@ import java.io.IOException;
 public class Servlet6 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //  通过请求对象，获取当前用户的HttpSession
         HttpSession s =  request.getSession();
+        // 通过HttpSession对象进行操作
         System.out.println(s.getAttribute("key1"));
         System.out.println(s.getAttribute("key2"));
         System.out.println(s.getAttribute("key3"));
