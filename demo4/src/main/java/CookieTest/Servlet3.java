6d39253e1fd14fb2b900a6143cb1bbeb26366243
闫兆祥 <963614893@qq.com>
2021-05-14 16:38:10 +0800

    demo

--- demo4/src/main/java/CookieTest/Servlet3.java
+++ demo4/src/main/java/CookieTest/Servlet3.java
@@ -9,13 +9,15 @@ import java.io.IOException;
 public class Servlet3 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //  创建cookie对象，保存共享数据
+        //  创建cookie对象，保存共享数据(默认情况下cookie生命周期为游览器的开始到关闭)
         Cookie cookie1 = new Cookie("key1","fuck you");
         Cookie cookie2 = new Cookie("key2","123");
+        cookie2.setMaxAge(60);//指定cookie2在硬盘上存活一分钟。(游览器关闭不影响cookie2的保存)
         //  将cookie写入到响应头，交给浏览器
         response.addCookie(cookie1);
         response.addCookie(cookie2);
         System.out.println(cookie1.toString());
         System.out.println(cookie2.toString());
+
     }
 }
--- demo4/src/main/java/CookieTest/Servlet4.java
+++ demo4/src/main/java/CookieTest/Servlet4.java
@@ -11,6 +11,7 @@ public class Servlet4 extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //  调用请求对象获取游览器返回的Cookie
         Cookie cookielist[] = request.getCookies();
+        //  遍历cookie集合
         for (Cookie cookie : cookielist){
             String key = cookie.getName();
             String value = cookie.getValue();
